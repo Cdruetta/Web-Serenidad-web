7111954ea2ed49cc5c1848de8b90d6df7e247c7e
Cdruetta <cristian.e.druetta@gmail.com>
2024-12-12 15:39:57 -0300

    arreglo del footer

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,46 +4,44 @@ import imagen1 from '../images/fondosImages/tibetanos.jpg';
 import imagen2 from '../images/fondosImages/palo_santo.jpg';
 import imagen3 from '../images/fondosImages/magicos.jpg';
 import imagen4 from '../images/fondosImages/conitos.jpg';
+import styles from './Carousel.module.css';
 
 function MyCarousel() {
   return (
-    <Carousel controls={false} indicators={false} interval={null}>
+    <Carousel controls={true} indicators={true} interval={3000} fade>
       <Carousel.Item>
-        <div style={{ display: 'flex', justifyContent: 'space-between', overflow: 'hidden' }}>
+        <div className={styles.carouselContainer}>
           <img
-            className="d-block"
+            className={styles.carouselImage}
             src={imagen1}
             alt="Imagen 1"
-            style={{
-              width: '25%',  // Ajusta el tamaño de las imágenes
-              marginRight: '10px', // Espacio entre las imágenes
-            }} 
           />
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className={styles.carouselContainer}>
           <img
-            className="d-block"
+            className={styles.carouselImage}
             src={imagen2}
             alt="Imagen 2"
-            style={{
-              width: '25%',
-              marginRight: '10px',
-            }} 
           />
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className={styles.carouselContainer}>
           <img
-            className="d-block"
+            className={styles.carouselImage}
             src={imagen3}
             alt="Imagen 3"
-            style={{
-              width: '25%',
-              marginRight: '10px',
-            }} 
           />
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className={styles.carouselContainer}>
           <img
-            className="d-block"
+            className={styles.carouselImage}
             src={imagen4}
             alt="Imagen 4"
-            style={{
-              width: '25%',
-            }} 
           />
         </div>
       </Carousel.Item>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,58 +1,63 @@
-import { Link } from 'react-router-dom'
-import { FaInstagram, FaFacebook} from 'react-icons/fa'
-import { CgMail } from 'react-icons/cg'
-import { CiLocationOn } from 'react-icons/ci'
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import { CgMail } from 'react-icons/cg';
+import { CiLocationOn } from 'react-icons/ci';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
 
-import BgHeader from "./Header.module.css"
-import bgFooter from './Footer.module.css'
+import BgHeader from "./Header.module.css";
+import bgFooter from './Footer.module.css';
 
-const iconsStyles = { color: 'white', fontSize: '50px', padding: '8px' }
+const iconsStyles = { color: 'white', fontSize: '50px', padding: '8px' };
 
 function Footer() {
-
     return (
         <>
-
             <div className={bgFooter.bgFooter}>
-
                 <div>
+                    {/* Sección de redes sociales */}
                     <div className={bgFooter.followConteiner}>
                         <h1 className={bgFooter.bgTitle}>Seguinos en nuestras redes sociales</h1>
-                        <a href="#/" target="_blank"><FaInstagram style={iconsStyles} /></a>
-                        <a href="#" target="_blank"><FaFacebook style={iconsStyles} /></a>
+                        <a
+                            href="https://www.instagram.com/serenidadriocuarto"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaInstagram style={iconsStyles} />
+                        </a>
+                        <a
+                            href="https://www.facebook.com/people/Serenidad/61559223890617"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaFacebook style={iconsStyles} />
+                        </a>
                     </div>
-                    <Container>
-                        <Row className={BgHeader.divisorVertical} >
 
-                            <Col><Link className={BgHeader.a} to="/">Home</Link></Col>
-                            <Col><Link className={BgHeader.a} to="/about-us"> Como comprar</Link></Col>
-                            <Col><Link className={BgHeader.a} to="/products"> Productos</Link></Col>
-                            <Col><Link className={BgHeader.a} to="/offers"> Preguntas frecuentes</Link></Col>
-                            
-
-                        </Row>
-                    </Container>
-                    <div className={bgFooter.followContact}>
-                        <CgMail style={iconsStyles} /><p className={bgFooter.bgSubtitle}> # </p>
-                        <CiLocationOn style={iconsStyles} /><p className={bgFooter.bgSubtitle}>Rio Cuarto, Cordoba - Vetan exclusiva online</p>
-                        
-                    </div>
-                    <div className={bgFooter.finally}>
-                            <Row>
-                                <Col>Copyright By GCsoft-2024</Col>
-                                <Col>Todos los derechos reservados</Col>
-                            </Row>
+                    {/* Contenedor principal */}
+                    <Container className={bgFooter.containerWithLines}>
+                        {/* Información de contacto */}
+                        <div className={bgFooter.followContact}>
+                            <CgMail style={iconsStyles} />
+                            <p className={bgFooter.bgSubtitle}>giseponzio@gmail.com</p>
+                            <CiLocationOn style={iconsStyles} />
+                            <p className={bgFooter.bgSubtitle}>Rio Cuarto, Cordoba - Venta exclusiva online</p>
                         </div>
+                    </Container>
+                    {/* Copyright */}
+                    <div className={bgFooter.finally}>
+                        <Row>
+                            <Col>
+                                Copyright By GCsoft-2024 - Todos los derechos reservados
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
             </div>
-
         </>
-
-    )
+    );
 }
 
 export default Footer;
